c0fc25f83532c2b6ee72d8507c48d10e0c255cd3
chobe111 <chobe1@naver.com>
2022-08-11 04:25:27 +0900

    Update Search Controller paht val to query string

--- src/domain/search/search.controller.ts
+++ src/domain/search/search.controller.ts
@@ -1,16 +1,13 @@
-import { Controller, Get, Headers, Param } from '@nestjs/common';
+import { Controller, Get, Headers, Param, Query } from '@nestjs/common';
 import { ApiCreatedResponse, ApiOperation, ApiPayloadTooLargeResponse, ApiTags } from '@nestjs/swagger';
-import { GetRecommendationVideoRequest } from 'src/types/api/request/recommendation/GetRecommendationVideo.request';
 import { GetRecommendationVideoResponse } from 'src/types/api/response/recommendation/GetRecommendationVideo.response';
-import { GetConditionSheetResponse } from 'src/types/api/response/sheet/GetConditionSheet.response';
-import { GetSheetResponse } from 'src/types/api/response/sheet/GetSheet.response';
 
 @Controller('v1/search')
 @ApiTags('검색 API')
 export class SearchController {
   constructor() {}
-  @Get('/:serachQuery')
+  @Get()
   @ApiOperation({ summary: '검색', description: 'query에 대한 곡 목록을 가져옵니다' })
   @ApiCreatedResponse({ type: GetRecommendationVideoResponse })
-  async getSerachResults(@Headers('Authorization') accessToken: string, @Param('searchQuery') searchQuery: string) {}
+  async getSerachResults(@Headers('Authorization') accessToken: string, @Query('searchTitle') searchTitle: string) {}
 }
--- src/types/api/response/recommendation/GetRecommendationVideo.response.ts
+++ src/types/api/response/recommendation/GetRecommendationVideo.response.ts
@@ -3,6 +3,6 @@ import { Video } from 'src/schemas/video.schema';
 import { Response } from '../response';
 
 export class GetRecommendationVideoResponse extends Response {
-  @ApiProperty({ description: '추천 곡 비디오 아이디 목록입니다' })
+  @ApiProperty({ description: '추천 곡 비디오 아이디 목록입니다', type: [Video] })
   data: Video[];
 }
